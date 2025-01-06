5cdd4d5dd69a4a5267411ddd5aef753581464659
ahmed-menrva <ahmed@menrvatechnologies.com>
2025-01-06 15:28:24 +0200

    freeze done

--- app.py
+++ app.py
@@ -4,22 +4,47 @@
 from sqlalchemy import create_engine
 import pandas as pd
 
-pwd = 'seif'  
+# pwd = 'seif'  
+# uid = 'root'    
+# host = "localhost"
+# port = 3306 
+# database = "seif"
+
+pwd = '123'  
 uid = 'root'    
 host = "localhost"
 port = 3306 
-database = "seif"
+database = "mart"
 
-mysql_user = 'root'
-mysql_password = '123'
-mysql_host = '127.0.0.1'
-mysql_database = 'mart'
+def load(df, tbl):
+    try:
+        # rows_imported = 0
+        connection_url = f'postgresql://{uid2}:{pwd2}@{host2}:{port2}/{database2}'
+        engine = create_engine(connection_url)
+        try:
+            src_conn = engine.connect()
+            print("Connection postgres successful")
+            table_name = 'stores'
+            df.to_sql(name=table_name, con=src_conn, if_exists="append", index=False)
+            print('done')
+            df_r = pd.read_sql_query(f"SELECT * FROM {table_name}", src_conn)
+            print(f"Data from {table_name}:\n", df_r)
+
+        except Exception as e:
+            raise Exception(f"Connection failed: {str(e)}")
+
+        # print(f'importing rows {rows_imported} to {rows_imported + len(df)}... for table {tbl}')
+        # df.to_sql(f'stg_{tbl}', engine, if_exists='replace', index=False, chunksize=100000)
+        # rows_imported += len(df)my
+        # print("Data imported successful")
+        # print('done')
+    except Exception as e:
+        print("Data load error: " + str(e))
 
 def extract():
     try:
-        # connection_url = f"mysql+pymysql://{uid}:{pwd}@{host}:{port}/{database}"
-        # src_engine = create_engine(connection_url)
-        src_engine = create_engine(f'mysql+mysqlconnector://{mysql_user}:{mysql_password}@{mysql_host}/{mysql_database}')  ###otifi
+        connection_url = f"mysql+pymysql://{uid}:{pwd}@{host}:{port}/{database}"
+        src_engine = create_engine(connection_url)
 
         try:
             src_conn = src_engine.connect()
@@ -38,7 +63,7 @@ def extract():
         for id, table_name in src_tables.items():
             df = pd.read_sql_query(f"SELECT * FROM {table_name}", src_conn)
             print(f"Data from {table_name}:\n", df)
-            # load(df, table_name)
+            load(df, table_name)
 
     except Exception as e:
         print(f"Data extract error: {str(e)}")
@@ -47,36 +72,13 @@ def extract():
             src_conn.close()
             print("Connection closed")
 
-uid2="etl"
-pwd2="admin"
+uid2="otifi"
+pwd2="123"
 host2 = "localhost"
 port2 = 5432  
-database2 = "iti"
+database2 = "porj"
 
-def load(df, tbl):
-    try:
-        # rows_imported = 0
-        connection_url = f'postgresql://{uid2}:{pwd2}@{host2}:{port2}/{database2}'
-        engine = create_engine(connection_url)
-        try:
-            src_conn = engine.connect()
-            print("Connection postgres successful")
-            table_name = 'student'
-            df.to_sql(name=table_name, con=src_conn, if_exists="append", index=False)
-            print('done')
-            df_r = pd.read_sql_query(f"SELECT * FROM {table_name}", src_conn)
-            print(f"Data from {table_name}:\n", df_r)
 
-        except Exception as e:
-            raise Exception(f"Connection failed: {str(e)}")
-
-        # print(f'importing rows {rows_imported} to {rows_imported + len(df)}... for table {tbl}')
-        # df.to_sql(f'stg_{tbl}', engine, if_exists='replace', index=False, chunksize=100000)
-        # rows_imported += len(df)my
-        # print("Data imported successful")
-        # print('done')
-    except Exception as e:
-        print("Data load error: " + str(e))
 
 try:
     extract()
