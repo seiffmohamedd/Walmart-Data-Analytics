be9abee225d8c0f9a63567f50a6e5ab2f596e7e5
ahmed-menrva <67879502+otifi3@users.noreply.github.com>
2025-01-10 21:28:21 +0200

    extraction and loading

---
+++ el.py
@@ -0,0 +1,56 @@
+from sqlalchemy import create_engine
+import pandas as pd
+from variables import *
+
+def extract_table_names(schema, src_dict):
+    try:
+        connection_url = f"mysql+pymysql://{src_dict['uid']}:{src_dict['pwd']}@{src_dict['host']}:{src_dict['port']}/{src_dict['database']}"
+        engine = create_engine(connection_url)
+        try:
+            src_conn = engine.connect()
+            print('Connection Established')
+            df_r = pd.read_sql_query(f"SELECT TABLE_NAME AS table_name FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '{schema}';", src_conn)
+            print('Data Extracted')
+            return list(df_r['table_name'])
+
+        except Exception as e:
+            raise Exception(f"Table Names Extraction Error: {str(e)}")
+    except Exception as e:
+        print(f"Connection Failed: {str(e)}")
+
+def extract(tbl, src_dict):
+    try:
+        connection_url = f"mysql+pymysql://{src_dict['uid']}:{src_dict['pwd']}@{src_dict['host']}:{src_dict['port']}/{src_dict['database']}"
+        engine = create_engine(connection_url)
+        try:
+            src_conn = engine.connect()
+            print('Connection Established')
+            df_r = pd.read_sql_query(f"SELECT * FROM {tbl}", src_conn)
+            print('Data Extracted')
+            return df_r
+
+        except Exception as e:
+            raise Exception(f"Data Extraction Error: {str(e)}")
+
+    except Exception as e:
+        print(f"Connection Failed: {str(e)}")
+
+
+def load(df, tbl, trgt_dict):
+    try:
+        connection_url = f'postgresql://{trgt_dict["uid2"]}:{trgt_dict["pwd2"]}@{trgt_dict["host2"]}:{trgt_dict["port2"]}/{trgt_dict["database2"]}'
+        engine = create_engine(connection_url)
+        try:
+            src_conn = engine.connect()
+            print("Connection Established")
+            df.to_sql(name=tbl, con=src_conn, if_exists="append", index=False)
+            print('Data Loaded')
+
+        except Exception as e:
+            raise Exception(f"Data Load Error: {str(e)}")
+
+    except Exception as e:
+        print(f"Connection Failed: {str(e)}")
+        
+tbl = 'products'
+load(extract(tbl, src_dict), tbl, trgt_dict)
---
+++ variables.py
@@ -0,0 +1,15 @@
+src_dict =dict(
+pwd = '123',  
+uid = 'root',    
+host = "localhost",
+port = 3306, 
+database = "mart")
+
+trgt_dict = dict(
+uid2="otifi",
+pwd2="123",
+host2 = "localhost",
+port2 = 5432,  
+database2 = "porj")
+
+schema_name = 'Mart'
